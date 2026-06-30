cfb0ad6b7372a666c17da5384f91f7b6c9c60d26
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-06-30 21:44:32 +0200

    emit

--- src/stub_build.c
+++ src/stub_build.c
@@ -10,14 +10,16 @@ t_stub	*stub_build(t_elf_ctx *ctx, t_crypto_ctx *crypto)
 	emit_lea_rip(&e, REG_RSI, &patch_lea_msg);   // offset réel inconnu pour l'instant
 	emit_mov_r32_imm32(&e, REG_RDX, WOODY_MSG_LEN);
 	emit_syscall(&e);
+	return (wrap_in_stub(&e, ctx));
+	
 	/* ... mprotect, boucle de déchiffrement ... */
-	emit_jmp_rel32(&e, &patch_jmp_oep);
+	//emit_jmp_rel32(&e, &patch_jmp_oep);
 
 	/* deuxième passe : patcher les offsets maintenant que tout est émis */
-	patch_disp32(&e, patch_lea_msg, msg_final_offset - (patch_lea_msg + 4));
-	patch_disp32(&e, patch_jmp_oep, oep_offset - (patch_jmp_oep + 4));
+	//patch_disp32(&e, patch_lea_msg, msg_final_offset - (patch_lea_msg + 4));
+	//patch_disp32(&e, patch_jmp_oep, oep_offset - (patch_jmp_oep + 4));
 
-	return (wrap_in_stub(&e, ctx));
+	//return (wrap_in_stub(&e, ctx));
 }
 
 void	stub_free(t_stub *stub)
