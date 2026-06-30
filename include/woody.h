820a5618c3ff0f4eaeaf08a18a35605c04d03fdf
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-06-30 20:10:29 +0200

    crypto

--- include/woody.h
+++ include/woody.h
@@ -7,6 +7,7 @@
 
 # include <sys/types.h>
 # include <sys/stat.h>
+# include <sys/random.h>
 
 # include <elf.h>
 # include <stdint.h>
--- src/crypto_rc4.c
+++ src/crypto_rc4.c
@@ -2,10 +2,29 @@
 
 int	crypto_generate_key(t_crypto_ctx *crypto)
 {
-	(void)crypto;
-	/* TODO: getrandom() ou /dev/urandom pour remplir crypto->key,
+	/* getrandom() ou /dev/urandom pour remplir crypto->key,
 	 * crypto->key_len = KEY_LEN */
-	return (-1);
+	ssize_t ret;
+
+	if (!crypto)
+		return (1);
+	crypto->key_len = KEY_LEN;
+	ret = getrandom(crypto->key, crypto->key_len, 0);
+	if (ret < 0 || (size_t)ret != crypto->key_len)
+	{
+		FILE *f;
+
+		f = fopen("/dev/urandom", "rb");
+		if (!f)
+			return (1);
+		if (fread(crypto->key, 1, crypto->key_len, f) != crypto->key_len)
+		{
+			fclose(f);
+			return (1);
+		}
+		fclose(f);
+	}
+	return (0);
 }
 
 void	rc4_apply(uint8_t *data, size_t len, const uint8_t *key, size_t key_len)
