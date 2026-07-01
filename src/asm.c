26d7660ab9fb252b52d95b46fb18bbfabc2217de
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-01 17:09:23 +0200

    test obfuscation decode

--- src/asm.c
+++ src/asm.c
@@ -202,7 +202,9 @@ static int	ainstr(t_asm *a, char toks[][64], int n)
 	if (!strcmp(toks[0], "sar") && n == 3)
 	{
 		DEBUG //
-		if (toks[1][0] != '[' && toks[2][0] != '[') {
+		if (toks[1][0] != '[' && toks[2][0] != '[')
+		{
+			DEBUG //
 			t_reg r1;
 			int size;
 			if (preg(toks[1], &r1, &size) && size == 32) {
@@ -247,21 +249,19 @@ static int	ainstr(t_asm *a, char toks[][64], int n)
 			emit_sar_mem_sib_imm8(&a->out->e, base, idx, (uint8_t)imm);
 			return 0;
 		}
-
-		// Cas 3: SAR [mem + disp], imm8 (ex: "sar [eax + 4], 3")
 		else if (toks[1][0] == '[' && toks[2][0] != '[' &&
 				(mt = pmem(toks[1], &base, &idx, lbl, &d8)) == 1 &&
 				preg(toks[2], &r2, &s2) && s2 == 8)
 		{
+			DEBUG //
 			emit_sar_mem_sib_imm8_disp(&a->out->e, base, idx, d8, (uint8_t)strtoll(toks[2], NULL, 0));
 			return 0;
 		}
-
-		// Cas 4: SAR r32, r32 (ex: "sar eax, cl")
 		else if (toks[1][0] != '[' && toks[2][0] != '[' &&
 				preg(toks[1], &r1, &s1) && s1 == 32 &&
 				preg(toks[2], &r2, &s2) && s2 == 8)
 		{
+			DEBUG //
 			emit_sar_r32_r32(&a->out->e, r1, r2);
 			return 0;
 		}
@@ -271,6 +271,7 @@ static int	ainstr(t_asm *a, char toks[][64], int n)
 				(mt = pmem(toks[1], &base, &idx, lbl, &d8)) == 1 &&
 				preg(toks[2], &r2, &s2) && s2 == 8)
 		{
+			DEBUG //
 			emit_sar_mem_r32(&a->out->e, base, idx, r2);
 			return 0;
 		}
