84e7b813b4457e30473f8f16c9fd5b7324e917bb
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-02 09:58:14 +0200

    decoder

--- src/stub_build.c
+++ src/stub_build.c
@@ -16,6 +16,20 @@
 */
 
 static const char STUB_SRC[] =
+	// preserves _dl_fini que le kernel passe dans rdx
+	"scan_start:\n"
+	"push rdx\n"
+
+	// write(1, MSG, 14)
+	"_SET eax, 1\n" //"mov eax, 1\n"
+	"_SET edi, 1\n" //"mov edi, 1\n"
+	//"mov eax, 1\n"
+	//"mov edi, 1\n"
+	"lea rsi, [msg]\n"
+	"mov edx, 14\n"
+	"syscall\n"
+
+	// ── lecture du payload ───────────────────────────────────────
     /* ── setup ───────────────────────────────────────────── */
     "xor ecx, ecx\n"          /* bit_index = 0 */
     "lea rsi, [scan_start]\n"
@@ -143,7 +157,77 @@ static const char STUB_SRC[] =
     "@lde_fallback:\n"
     "add rsi, 1\n" "jmp @lde_loop\n"
 
-    "@lde_done:\n";
+    "@lde_done:\n"
+
+	// Decrypt payload (RC4) et jump vers OEP
+
+	// mprotect(page_vaddr, page_size, PROT_RWX)
+	"mov rdi, prot_addr\n"
+	"mov esi, prot_size\n"
+	"mov edx, 7\n"
+	"mov eax, 10\n"
+	"syscall\n"
+
+	// S-box sur la pile
+	"sub rsp, 256\n"
+
+	// rsi = base de la cle (RIP-relative)
+	"lea rsi, [key]\n"
+
+	// KSA init : S[i] = i, i = 0..255
+	"xor ecx, ecx\n"
+	"@ksa_init:\n"
+	"mov [rsp+rcx], cl\n"
+	"inc cl\n"
+	"jnz @ksa_init\n"
+
+	// KSA : melange S par la cle
+	"xor ecx, ecx\n"
+	"xor edx, edx\n"
+	"@ksa_loop:\n"
+	"movzx eax, [rsp+rcx]\n"
+	"add dl, al\n"
+	"mov al, cl\n"
+	"and al, key_mask\n"
+	"movzx eax, al\n"
+	"movzx eax, [rsi+rax]\n"
+	"add dl, al\n"
+	"movzx eax, [rsp+rcx]\n"
+	"xchg [rsp+rdx], al\n"
+	"mov [rsp+rcx], al\n"
+	"inc cl\n"
+	"jnz @ksa_loop\n"
+
+	// PRGA + XOR payload
+	"mov rdi, text_vaddr\n"
+	"xor ecx, ecx\n"
+	"xor edx, edx\n"
+	"xor ebx, ebx\n"
+	"@prga_loop:\n"
+	"inc cl\n"
+	"movzx eax, [rsp+rcx]\n"
+	"add dl, al\n"
+	"xchg [rsp+rdx], al\n"
+	"mov [rsp+rcx], al\n"
+	"add al, [rsp+rdx]\n"
+	"movzx eax, al\n"
+	"movzx eax, [rsp+rax]\n"
+	"xor [rdi+rbx], al\n"
+	"inc rbx\n"
+	"cmp ebx, text_len\n"
+	"jl @prga_loop\n"
+
+	// restauration et saut vers OEP
+	"add rsp, 256\n"
+	"pop rdx\n"
+	"jmp @oep\n"
+	"scan_end:\n"
+
+	// donnees embarquees apres le code
+	"msg:\n"
+	".msg\n"
+	"key:\n"
+	".key\n";
 /*
     "push rdx\n"
     "_SET eax, 1\n"
@@ -205,17 +289,6 @@ static const char STUB_SRC[] =
 */
 
 /*
-	// preserves _dl_fini que le kernel passe dans rdx
-	"push rdx\n"
-
-	// write(1, MSG, 14)
-	"_SET eax, 1\n" //"mov eax, 1\n"
-	"_SET edi, 1\n" //"mov edi, 1\n"
-	//"mov eax, 1\n"
-	//"mov edi, 1\n"
-	"lea rsi, [msg]\n"
-	"mov edx, 14\n"
-	"syscall\n"
 
 	////////??/////////////////////////////////
     // ── setup ─────────────────────────────────────────────
@@ -282,72 +355,6 @@ static const char STUB_SRC[] =
     "push rcx\n"
 ////////////////////////////////////////////
 
-	// mprotect(page_vaddr, page_size, PROT_RWX)
-	"mov rdi, prot_addr\n"
-	"mov esi, prot_size\n"
-	"mov edx, 7\n"
-	"mov eax, 10\n"
-	"syscall\n"
-
-	// S-box sur la pile
-	"sub rsp, 256\n"
-
-	// rsi = base de la cle (RIP-relative)
-	"lea rsi, [key]\n"
-
-	// KSA init : S[i] = i, i = 0..255
-	"xor ecx, ecx\n"
-	"@ksa_init:\n"
-	"mov [rsp+rcx], cl\n"
-	"inc cl\n"
-	"jnz @ksa_init\n"
-
-	// KSA : melange S par la cle
-	"xor ecx, ecx\n"
-	"xor edx, edx\n"
-	"@ksa_loop:\n"
-	"movzx eax, [rsp+rcx]\n"
-	"add dl, al\n"
-	"mov al, cl\n"
-	"and al, key_mask\n"
-	"movzx eax, al\n"
-	"movzx eax, [rsi+rax]\n"
-	"add dl, al\n"
-	"movzx eax, [rsp+rcx]\n"
-	"xchg [rsp+rdx], al\n"
-	"mov [rsp+rcx], al\n"
-	"inc cl\n"
-	"jnz @ksa_loop\n"
-
-	// PRGA + XOR payload
-	"mov rdi, text_vaddr\n"
-	"xor ecx, ecx\n"
-	"xor edx, edx\n"
-	"xor ebx, ebx\n"
-	"@prga_loop:\n"
-	"inc cl\n"
-	"movzx eax, [rsp+rcx]\n"
-	"add dl, al\n"
-	"xchg [rsp+rdx], al\n"
-	"mov [rsp+rcx], al\n"
-	"add al, [rsp+rdx]\n"
-	"movzx eax, al\n"
-	"movzx eax, [rsp+rax]\n"
-	"xor [rdi+rbx], al\n"
-	"inc rbx\n"
-	"cmp ebx, text_len\n"
-	"jl @prga_loop\n"
-
-	// restauration et saut vers OEP
-	"add rsp, 256\n"
-	"pop rdx\n"
-	"jmp @oep\n"
-
-	// donnees embarquees apres le code
-	"msg:\n"
-	".msg\n"
-	"key:\n"
-	".key\n";
 	*/
 
 t_stub	*stub_build(t_elf_ctx *ctx, t_crypto_ctx *crypto)
