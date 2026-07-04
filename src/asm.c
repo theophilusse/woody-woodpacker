9624793d0ecc8e6a2e6769bc81f239d1a8ae803a
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-04 17:29:17 +0200

    fix LDE debug

--- src/asm.c
+++ src/asm.c
@@ -1,5 +1,8 @@
 #include "asm.h"
 
+static int g_bit_log[512];
+static int g_bit_log_len = 0;
+
 static const char *R64[] = {"rax","rcx","rdx","rbx","rsp","rbp","rsi","rdi",
                              "r8","r9","r10","r11","r12","r13","r14","r15"};
 static const char *R32[] = {"eax","ecx","edx","ebx","esp","ebp","esi","edi",
