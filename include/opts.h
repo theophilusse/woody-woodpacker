a4119080221d29d02ea562769d426ffc962aad96
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-06 12:57:00 +0200

    args + verbose

--- include/opts.h
+++ include/opts.h
@@ -4,15 +4,6 @@
 # include "woody.h"
 # include <getopt.h>
 
-static struct option long_opts[] = {
-    { "help",   no_argument, 0, '?' },
-    { "verbose", no_argument, 0, 'v' },
-    { "antidebug",    required_argument, 0, 'd'  },
-    { "int3trap",   required_argument, 0, 'i'   },
-    { "lde", required_argument, 0, 'l'  },
-    { 0, 0, 0, 0 }  // terminateur de tableau pour getopt_long
-};
-
 t_opts  parse_args(int argc, char **argv);
 
 #endif
--- src/parse_args.c
+++ src/parse_args.c
@@ -1,5 +1,14 @@
 #include "woody.h"
 
+static struct option long_opts[] = {
+    { "help",   no_argument, 0, '?' },
+    { "verbose", no_argument, 0, 'v' },
+    { "antidebug",    required_argument, 0, 'd'  },
+    { "int3trap",   required_argument, 0, 'i'   },
+    { "lde", required_argument, 0, 'l'  },
+    { 0, 0, 0, 0 }  // terminateur de tableau pour getopt_long
+};
+
 struct s_opts default_opts(void)
 {
 	struct s_opts opts;
