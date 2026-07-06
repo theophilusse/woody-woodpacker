6eba82b1bb210863b2cadb4dd965e57ed976470f
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-06 12:35:39 +0200

    args + verbose

--- include/opts.h
+++ include/opts.h
@@ -3,21 +3,6 @@
 
 #include <getopt.h>
 
-typedef struct s_opts {
-    int     verbose;      // -v
-    char    *iface;       // -i
-    int     max_ttl;      // -m
-    int     port;         // -p
-    char    *src_addr;    // -s
-    int     n_queries;    // -q
-    int     s_queries;    // -N
-    int     tos;          // -t
-    int     flow_label;   // -l
-    char    *hostname;    // hostname
-    struct in_addr local_ip; // adresse IP locale
-    char    *iface_name;
-} t_opts;
-
 typedef struct s_opts
 {
     int     verbose;
@@ -30,14 +15,9 @@ typedef struct s_opts
 static struct option long_opts[] = {
     { "help",   no_argument, 0, '?' },
     { "verbose", no_argument, 0, 'v' },
-    { "iface",    required_argument, 0, 'i'  },
-    { "max_ttl",   required_argument, 0, 'm'   },
-    { "port", required_argument, 0, 'p'  },
-    { "src_addr", required_argument, 0, 's'  },
-    { "n_queries", required_argument, 0, 'q'  },
-    { "s_queries", required_argument, 0, 'N'  },
-    { "tos", required_argument, 0, 't'  },
-    { "flow_label", required_argument, 0, 'l'  },
+    { "antidebug",    required_argument, 0, 'd'  },
+    { "int3trap",   required_argument, 0, 'i'   },
+    { "lde", required_argument, 0, 'l'  },
     { 0, 0, 0, 0 }  // terminateur de tableau pour getopt_long
 };
 
