5da58fba910d47cbbe6ddfc414cefb756142d9af
Theophile Trosseau <theophile.trosseau@laposte.net>
2026-07-07 21:47:47 +0200

    stub av detect

--- resources/syscall.c
+++ resources/syscall.c
@@ -1,17 +1,34 @@
-#include <sys/syscall.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 
 int main(void)
 {
-    long fd = syscall(SYS_open, "/proc", O_RDONLY, 0);
+    long fd;
+    asm volatile (
+        "mov $2, %%rax\n"
+        "lea proc_path(%%rip), %%rdi\n"
+        "mov $0, %%rsi\n"
+        "syscall\n"
+        : "=a"(fd)
+        :
+        : "rdi", "rsi"
+    );
     printf("fd=%ld\n", fd);
 
     char buf[4096];
-    long n = syscall(SYS_getdents64, fd, buf, 4096);
+    long n;
+    asm volatile (
+        "mov $78, %%rax\n"
+        "mov %1, %%rdi\n"
+        "mov %2, %%rsi\n"
+        "mov $4096, %%rdx\n"
+        "syscall\n"
+        : "=a"(n)
+        : "r"(fd), "r"(buf)
+        : "rdi", "rsi", "rdx"
+    );
     printf("getdents64=%ld\n", n);
-    if (n < 0)
-        perror("getdents64");
+
     return 0;
 }
+static const char proc_path[] = "/proc";
